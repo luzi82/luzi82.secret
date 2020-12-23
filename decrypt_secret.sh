6a24f65e42f6e72b13f82a18e35a388d4e6c04b3
luzi82 <luzi82@gmail.com>
2020-12-23 06:59:08 +0000

    script use abs path

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -e
 
+PROJECT_ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# secret
+cd ${PROJECT_ROOT_PATH}
 if [ -f master/SECRET ]; then
-  SECRET=`cat master/SECRET`
+  SECRET=`cat ${PROJECT_ROOT_PATH}/master/SECRET`
 else
   read -sp "SECRET: " SECRET
   echo
 fi
 
 # rm exising secret
-rm -rf secret
+cd ${PROJECT_ROOT_PATH}
+rm -rf ${PROJECT_ROOT_PATH}/secret
 
 # verify | decrypt | untar
-gpg --no-default-keyring --keyring ${PWD}/public-key.gpg --decrypt secret.tar.gz.gpg.sig | \
+cd ${PROJECT_ROOT_PATH}
+gpg \
+  --no-default-keyring \
+  --keyring ${PROJECT_ROOT_PATH}/public-key.gpg \
+  --decrypt ${PROJECT_ROOT_PATH}/secret.tar.gz.gpg.sig | \
 gpg --quiet --batch --yes --decrypt --passphrase="${SECRET}" | \
 tar xzf -
--- encrypt_master.sh
+++ encrypt_master.sh
@@ -1,19 +1,34 @@
 #!/bin/bash -e
 
+PROJECT_ROOT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# tmp
 TMP_PATH=`mktemp -d`
 trap "{ rm -rf ${TMP_PATH}; }" EXIT
 
 # rm old pack file
-rm -rf master.tar.gz.gpg.sig
+rm -rf ${PROJECT_ROOT_PATH}/master.tar.gz.gpg.sig
 
 # create gpg folder
 mkdir -p ${TMP_PATH}/gpg
 chmod 700 ${TMP_PATH}/gpg
 
 # import private key
-gpg --homedir ${TMP_PATH}/gpg --batch --import master/private-key.asc
+gpg \
+  --homedir ${TMP_PATH}/gpg \
+  --batch \
+  --import ${PROJECT_ROOT_PATH}/master/private-key.asc
 
 # tar | encrypt | sign
 tar czf - master | \
-gpg --symmetric --cipher-algo AES256 --pinentry-mode loopback --passphrase-file master/MASTER_SECRET | \
-gpg --homedir ${TMP_PATH}/gpg --batch --pinentry-mode loopback --passphrase-file master/MASTER_SECRET --output master.tar.gz.gpg.sig --sign
+gpg \
+  --symmetric \
+  --cipher-algo AES256 \
+  --pinentry-mode loopback \
+  --passphrase-file ${PROJECT_ROOT_PATH}/master/MASTER_SECRET | \
+gpg \
+  --homedir ${TMP_PATH}/gpg \
+  --batch \
+  --pinentry-mode loopback \
+  --passphrase-file ${PROJECT_ROOT_PATH}/master/MASTER_SECRET \
+  --output ${PROJECT_ROOT_PATH}/master.tar.gz.gpg.sig --sign
